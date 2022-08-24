4a91eaf6b89a51a71a9e10fde2104ec9546a3920
Nitish Mishra <105452945+nitish0307@users.noreply.github.com>
2022-08-24 19:04:34 +0530

    Add files via upload

---
+++ insertingelement_in_array.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    int i;
+    for(i=0; i<n; i++)
+    {
+        cin >> arr[i];
+
+    }
+    int pos;
+    cin >> pos;
+    int ele;
+    cin >> ele;
+    if(pos>n)
+    cout << "Invalid Input";
+    else{
+        for(i =n -1 ; i>= pos - 1 ; i--)
+        arr[i+1] = arr[i];
+        cout << "Array after Insertion is:\n";
+
+        for (i =0 ; i<= n; i++)
+        cout << arr[i] << " ";
+
+    }
+    return 0;
+}
